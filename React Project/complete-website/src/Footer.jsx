10d4cae466a6a5374a36e5e9e1db4fa0800d1c6f
rj <69528706+rj-riddhi@users.noreply.github.com>
2022-10-14 11:55:45 +0530

    LAST COMMIT

--- React Project/complete-website/src/Footer.jsx
+++ React Project/complete-website/src/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 const Footer = () => {
     return (
         <>
-            <footer className="text-center bg-light w-100">
+            <Footer className="text-center bg-white ">
                 <p>© 2022 TechRiddhi. All Rights Reserved | Terms and Condition</p>
-            </footer>
+            </Footer>
         </>
     )
 }
