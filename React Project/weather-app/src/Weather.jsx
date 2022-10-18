d2dbca0f7f74e93bd46a1098a4b641da9fd95d56
rj <69528706+rj-riddhi@users.noreply.github.com>
2022-10-18 18:56:29 +0530

    completed

--- React Project/weather-app/src/Weather.jsx
+++ React Project/weather-app/src/Weather.jsx
@@ -1,50 +1,58 @@
 import React, { useEffect, useState } from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 const Weather = () => {
-
-    const [data, setCity] = useState({
-        city: 'Ahmedabad',
-        tmp: '',
-        min: '',
-        max: ''
-    });
-    const getCity = (e) => {
-        setCity({
-         city: e.target.value
-        })
-    }
-    useEffect(()=>{
-        const fetchApi =async () => {
-            const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${data.city}?unitGroup=us&key=3CZAJAZK436ECGDS2FWH2TE6P&contentType=json`;
-            const response =await fetch(url)
-            const jsonData =await response.json();console.log(jsonData)
-            setCity({
-             city: jsonData.address,
-            tmp: calcuis(jsonData.currentConditions.temp)
-            });
-        }
-        fetchApi();
-    },[getCity])
-
-    const calcuis = (temp) => {
-      return ((temp - 32) * 5/9);
-    }
+  const [data, setData] = useState(null);
+  const [search, setSearch] = useState("Ahmedabad");
+  useEffect(() => {
+    const fetchApi = async () => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=d86deb0cb24a5ab7fbbdfd69360838eb&units=metric`;
+      const response = await fetch(url);
+      const jsonData = await response.json();
+      setData(jsonData.main);
+    };
+    fetchApi();
+  }, [search]);
   return (
     <>
       <div className="container">
         <div className="row justify-content-center">
-          {/* <div className="main_div col-md-4 col-10  mx-auto"> */}
           <div className="main-div">
             <div className="row mx-auto ">
-              <input type="search" className="col-4 mx-auto input-div" onChange={getCity}/>
+              <input
+                type="search"
+                className="col-4 mx-auto input-div"
+                onChange={(e) => {
+                  setSearch(e.target.value);
+                }}
+                value = {search}
+              />
             </div>
-            <div className="information">
-              <h1>
-                <i className="fa fa-street-view street-icon" aria-hidden="true"></i> {data.city}
-              </h1>
-              <h2>{data.tmp}°Cel</h2>
-              <h3>Min : {data.min}°Cel | Max : {data.max}°Cel</h3>
-            </div>
+            {!data ? (
+               
+                 search !== '' ?
+                  <div
+                    className="spinner-border text-secondary m-5 "
+                    role="status"
+                  >
+                  </div> : <p className="mt-5">Search Something</p>
+                
+            ) : (
+              <div className="information">
+                <h1>
+                  <i
+                    className="fa fa-street-view street-icon"
+                    aria-hidden="true"
+                  ></i>
+                  {search}
+                </h1>
+                <h2>{data.temp}°Cel</h2>
+                <h3>
+                  Min : {data.temp_min}°Cel | Max : {data.temp_max}
+                  °Cel
+                </h3>
+              </div>
+            )}
+
             <div className="wave -one"></div>
             <div className="wave -two"></div>
             <div className="wave -three"></div>
